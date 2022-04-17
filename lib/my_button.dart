c976ab82e90a810eb5289c562c0710a10db87069
Jun Lee <bozloo@gmail.com>
2022-04-17 00:15:20 -0500

    refactory with my button method

---
+++ lib/my_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Widget image;
+  final Widget text;
+  final Color color;
+  final double radius;
+  final VoidCallback onPressed;
+
+  const MyButton({Key? key,
+    required this.image,
+    required this.text,
+    required this.color,
+    required this.radius,
+    required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      height: 50.0,
+      child: ElevatedButton(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            image,
+            text,
+            Opacity(
+              opacity: 0.0,
+              child: image,
+            ),
+          ],
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: color,
+          minimumSize: Size(100, 50),
+        ),
+        onPressed: onPressed,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(radius),
+        ),
+      ),
+    );
+  }
+}
